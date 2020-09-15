1a286c0972af98c89985e63bf38d40d282a07afa
sten4eg <buros1122@gmail.com>
2020-09-15 23:54:47 +0300

    refactor requestauthlogin.go

--- loginserver/clientpackets/requestauthlogin.go
+++ loginserver/clientpackets/requestauthlogin.go
@@ -1,10 +1,6 @@
 package clientpackets
 
 import (
-	"crypto/rsa"
-
-	_ "github.com/andreburgaud/crypt2go/ecb"
-	_ "github.com/andreburgaud/crypt2go/padding"
 	"l2goserver/loginserver/models"
 	"math/big"
 )
@@ -26,24 +22,4 @@ func NewRequestAuthLogin(request []byte, client models.Client) RequestAuthLogin 
 	result.Password = string(decodeData[14:28])
 
 	return result
-}
-func PublicDecrypt(pubKey *rsa.PublicKey, data []byte) ([]byte, error) {
-	c := new(big.Int)
-	m := new(big.Int)
-	m.SetBytes(data)
-	e := big.NewInt(int64(pubKey.E))
-	c.Exp(m, e, pubKey.N)
-	out := c.Bytes()
-	skip := 0
-	for i := 2; i < len(out); i++ {
-		if i+1 >= len(out) {
-			break
-		}
-		if out[i] == 0xff && out[i+1] == 0 {
-			skip = i + 2
-			break
-		}
-	}
-
-	return out[skip:], nil
 }
