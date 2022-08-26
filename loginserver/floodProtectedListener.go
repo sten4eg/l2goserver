0c11d2d86e27b607d96684dacdad36e3ac17da56
User Name <probotpro47@yandex.by>
2022-08-27 00:45:29 +0300

    Update AcceptTCPWithFloodProtection

--- loginserver/floodProtectedListener.go
+++ loginserver/floodProtectedListener.go
@@ -10,12 +10,14 @@ import (
 const fastConnectionLimit = 15
 const normalConnectionTime = 700
 const fastConnectionTime = 350
-const maxConnectionPerIP = 50
+const maxConnectionPerIP = 2
+const banTime = time.Minute
 
 type connection struct {
 	connNum    uint8
 	lastConn   int64
 	isFlooding bool
+	banExpire  time.Time
 }
 
 var floodProtection *xsync.MapOf[*connection]
@@ -35,23 +37,39 @@ func (ls *LoginServer) AcceptTCPWithFloodProtection() (*net.TCPConn, error) {
 		fConn, ok := floodProtection.Load(x)
 
 		if !ok {
-			floodProtection.Store(x, &connection{1, time.Now().UnixMilli(), false})
+			floodProtection.Store(x, &connection{1, time.Now().UnixMilli(), false, time.Now()})
 		} else {
+			if fConn.isFlooding {
+				if time.Now().After(fConn.banExpire) {
+					fConn.isFlooding = false
+					fConn.banExpire = time.Now()
+					fConn.connNum = 1
+					fConn.lastConn = time.Now().UnixMilli()
+				} else {
+					_ = conn.Close()
+					continue
+				}
+			}
+
 			fConn.connNum++
 			curTime := time.Now().UnixMilli()
 			connectionTime := curTime - fConn.lastConn
 			fConn.lastConn = curTime
 
-			if (fConn.connNum > fastConnectionLimit && connectionTime < normalConnectionTime) ||
-				connectionTime < fastConnectionTime ||
-				fConn.connNum > maxConnectionPerIP {
-				_ = conn.Close()
-				fConn.connNum--
-				fConn.isFlooding = true
-				continue
-			}
+			if (fConn.connNum > 2 && connectionTime < fastConnectionTime) ||
+				fConn.connNum > maxConnectionPerIP || connectionTime < normalConnectionTime {
+				if connectionTime > 5000 {
+					fConn.isFlooding = false
+					fConn.banExpire = time.Now()
+					fConn.connNum = 1
+				} else {
+					_ = conn.Close()
+					fConn.isFlooding = true
+					fConn.banExpire = time.Now().Add(banTime)
+					continue
+				}
 
-			fConn.isFlooding = false
+			}
 		}
 		return conn, nil
 	}
