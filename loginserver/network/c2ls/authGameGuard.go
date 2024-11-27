0d178e0ccdbdacbea1f6b9df1a7df726ac0a675e
sten4eg <buros1122@gmail.com>
2024-11-28 00:28:53 +0300

    -2

--- loginserver/network/c2ls/authGameGuard.go
+++ loginserver/network/c2ls/authGameGuard.go
@@ -2,14 +2,18 @@ package c2ls
 
 import (
 	"errors"
-	"l2goserver/loginserver/models"
 	"l2goserver/loginserver/network/ls2c"
 	"l2goserver/packets"
 )
 
 var errWrongSession = errors.New("sessionId не совпал")
 
-func NewAuthGameGuard(request []byte, ctx *models.ClientCtx) error {
+type authGameGuardInterface interface {
+	GetSessionId() uint32
+	Send([]byte) error
+}
+
+func NewAuthGameGuard(request []byte, ctx authGameGuardInterface) error {
 	var sessionId uint32
 	var packet = packets.NewReader(request)
 
